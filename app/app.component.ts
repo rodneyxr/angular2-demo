b2b84ba4db7d868a36976bdc9074354c809f3424
Rodney Rodriguez <rodneyxr@gmail.com>
2016-07-15 09:25:53 -0500

    Remove template code from typescript file

--- app/app.component.ts
+++ app/app.component.ts
@@ -2,27 +2,41 @@
  * Created by Rodney Rodriguez on 7/13/2016.
  */
 
-import {Component} from '@angular/core';
+import {Component} from "@angular/core";
 
 export class Employee {
     id:number;
     name:string;
 }
 
+const EMPLOYEES:Employee[] = [
+    {id: 10, name: 'Tim Duncan'},
+    {id: 11, name: 'Tony Parker'},
+    {id: 12, name: 'Manu Ginobili'},
+    {id: 13, name: 'LaMarcus Aldridge'},
+    {id: 14, name: 'Danny Green'},
+    {id: 15, name: 'Jonathan Simmons'},
+    {id: 16, name: 'Kyle Anderson'},
+    {id: 17, name: 'Kevin Martin'},
+    {id: 18, name: 'Ryan Arcidiacono'},
+    {id: 19, name: 'Pau Gasol'},
+    {id: 20, name: 'Dejounte Murray'},
+];
+
 @Component({
     selector: 'my-app',
-    template: `
-        <h1>{{title}}</h1>
-        <h2>{{employee.name}}&#39;s details!</h2>
-        <div><label>ID: </label>{{employee.id}}</div>
-        <div><input [(ngModel)]="employee.name" placeholder="name"></div>
-    `
+    moduleId: module.id,
+    templateUrl: 'app.component.html',
+    styleUrls: ['app.component.css']
 })
 
 export class AppComponent {
     title = 'Company Employees';
-    employee:Employee = {
-        id: 1,
-        name: 'Rodney'
-    };
+    employees = EMPLOYEES;
+    selectedEmployee:Employee;
+
+    onSelect(employee:Employee) {
+        this.selectedEmployee = employee;
+    }
 }
+
